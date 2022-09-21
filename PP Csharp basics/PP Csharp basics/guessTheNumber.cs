65a1f98e3574da03cb77f56bf168889e9a1b2447
Yalchin403 <yalchin.mammadli.std@bhos.edu.az>
2022-09-21 04:31:24 +0400

    first guessNumber console app added

---
+++ PP Csharp basics/PP Csharp basics/guessTheNumber.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int secretNumber = random.Next(0, 100);
+int userInput;
+
+//TODO:
+// need to add counter (int)
+// need to add counter (time)
+do
+{
+    Console.WriteLine(secretNumber);
+    Console.WriteLine("Guess the number");
+    userInput = int.Parse(Console.ReadLine());
+
+    if (secretNumber == userInput)
+    {
+        Console.WriteLine("Found it!");
+    }
+    else if (secretNumber > userInput)
+    {
+        Console.WriteLine("Please provide a greater number...");
+    }
+
+    else
+    {
+        Console.WriteLine("Please provide a smaller number...");
+    }
+}
+
+while (userInput != secretNumber);
